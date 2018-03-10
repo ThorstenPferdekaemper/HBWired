c32f49b52786fc06ab06fec5bfa248bbe082beb9
loetmeister <loetmeister@habmalnefrage.de>
2018-03-10 12:14:15 +0100

    adaption to new 'afterReadConfig'

--- HBW-Sen-Key-12/HBW-Sen-Key-12.ino
+++ HBW-Sen-Key-12/HBW-Sen-Key-12.ino
@@ -74,7 +74,7 @@ class HBSenKey : public HBWChannel {
   public:
     HBSenKey(uint8_t _pin, hbw_config_key* _config);
     virtual void loop(HBWDevice*, uint8_t channel);
-    void afterReadConfig();
+    virtual void afterReadConfig();
   private:
     uint8_t pin;   // Pin
     uint32_t lastSentLong;      // Zeit, zu der das letzte Mal longPress gesendet wurde
@@ -90,33 +90,24 @@ HBSenKey* keys[NUM_CHANNELS];
 class HBSenDevice : public HBWDevice {
     public: 
     HBSenDevice(uint8_t _devicetype, uint8_t _hardware_version, uint16_t _firmware_version,
-            Stream* _rs485, uint8_t _txen, 
-            uint8_t _configSize, void* _config, 
-        uint8_t _numChannels, HBWChannel** _channels,
-        Stream* _debugstream, HBWLinkSender* linksender = NULL, HBWLinkReceiver* linkreceiver = NULL) :
-          HBWDevice(_devicetype, _hardware_version, _firmware_version,
-            _rs485, _txen, _configSize, _config, _numChannels, ((HBWChannel**)(_channels)),
-            _debugstream, linksender, linkreceiver) {
-      // looks like virtual methods are not properly called here
-      afterReadConfig();        
+                Stream* _rs485, uint8_t _txen, 
+                uint8_t _configSize, void* _config, 
+                uint8_t _numChannels, HBWChannel** _channels,
+                Stream* _debugstream, HBWLinkSender* linksender = NULL, HBWLinkReceiver* linkreceiver = NULL) :
+    HBWDevice(_devicetype, _hardware_version, _firmware_version,
+              _rs485, _txen, _configSize, _config, _numChannels, ((HBWChannel**)(_channels)),
+              _debugstream, linksender, linkreceiver) {
     };
-
-    void afterReadConfig() {
-        // defaults setzen
-        if(hbwconfig.logging_time == 0xFF) hbwconfig.logging_time = 20;
-        // if(config.central_address == 0xFFFFFFFF) config.central_address = 0x00000001;
-        for(uint8_t channel = 0; channel < NUM_CHANNELS; channel++){
-            if(hbwconfig.keys[channel].long_press_time == 0xFF) 
-                hbwconfig.keys[channel].long_press_time = 10;
-            keys[channel]->afterReadConfig();    
-        };
-    };
+    virtual void afterReadConfig();
 };
 
 
 HBSenDevice* device = NULL;
 
-
+// device specific defaults
+void HBSenDevice::afterReadConfig() {
+  if(hbwconfig.logging_time == 0xFF) hbwconfig.logging_time = 20;
+};
 
 HBSenKey::HBSenKey(uint8_t _pin, hbw_config_key* _config) 
               : config(_config), pin(_pin), 
@@ -124,6 +115,7 @@ HBSenKey::HBSenKey(uint8_t _pin, hbw_config_key* _config)
 };
 
 void HBSenKey::afterReadConfig(){
+    if(config->long_press_time == 0xFF) config->long_press_time = 10;
     button = ClickButton(pin, config->inverted ? LOW : HIGH, config->pullup ? HIGH : LOW);
     button.debounceTime   = 20;   // Debounce timer in ms
     button.multiclickTime = 250;  // Time limit for multi clicks
@@ -193,7 +185,7 @@ void setup()
                          &Serial,
                          new HBWLinkKey(NUM_LINKS,LINKADDRESSSTART), NULL);
 
-  device->setConfigPins();  // 8 and 13 is the default
+  device->setConfigPins(BUTTON, LED);  // 8 and 13 is the default
  
   hbwdebug(F("B: 2A "));
   hbwdebug(freeRam());
