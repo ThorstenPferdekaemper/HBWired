4ffc2df7654e3fbad6af31ed82eab0df1f9dbaf8
loetmeister <loetmeister@habmalnefrage.de>
2021-01-20 20:56:56 +0100

    increment version for recent powersave/sleep addition

--- HBW-CC-DT3-T6/HBW-CC-DT3-T6.ino
+++ HBW-CC-DT3-T6/HBW-CC-DT3-T6.ino
@@ -21,7 +21,7 @@
 
 
 #define HARDWARE_VERSION 0x01
-#define FIRMWARE_VERSION 0x0028
+#define FIRMWARE_VERSION 0x0029
 #define HMW_DEVICETYPE 0x9C //device ID (make sure to import .xml into FHEM)
 
 #define NUMBER_OF_TEMP_CHAN 6   // input channels - 1-wire temperature sensors
