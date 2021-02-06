2bd7441c434d39ec0e5b5e065193ddcc5302e4af
loetmeister <loetmeister@habmalnefrage.de>
2021-02-06 20:23:37 +0100

    move backlight to own lib - HBWDimBacklight.h

--- HBW-DIS-Key-4/HBWDisplayLCD.cpp
+++ HBW-DIS-Key-4/HBWDisplayLCD.cpp
@@ -7,11 +7,12 @@
  */
  
 #include "HBWDisplayLCD.h"
+#include "HBWDimBacklight.h"
 #include <EEPROM.h>
 
 
 /* global/static */
-boolean HBWDisplayDim::displayWakeUp = true;  // wakeup on start - if config->startup allows
+//boolean HBWDimBacklight::displayWakeUp = true;  // wakeup on start - if config->startup allows
 byte HBWDisplayVChannel::numTotal = 0;
 byte HBWDisplayLine::numTotal = 0;
 
@@ -38,16 +39,16 @@ HBWDisplayVChBool::HBWDisplayVChBool(hbw_config_displayVChBool* _config)
 };
 
 
-HBWDisplayDim::HBWDisplayDim(hbw_config_display_backlight* _config, uint8_t _backlight_pin, uint8_t _photoresistor_pin)
-{
-  config = _config;
-  backlightPin = _backlight_pin;
-  photoresistorPin = _photoresistor_pin;
-  backlightLastUpdate = 0;
-  lastKeyNum = 0;
-  initDone = false;
-  currentValue = 0;
-};
+//HBWDisplayDim::HBWDisplayDim(hbw_config_display_backlight* _config, uint8_t _backlight_pin, uint8_t _photoresistor_pin)
+//{
+//  config = _config;
+//  backlightPin = _backlight_pin;
+//  photoresistorPin = _photoresistor_pin;
+//  backlightLastUpdate = 0;
+//  lastKeyNum = 0;
+//  initDone = false;
+//  currentValue = 0;
+//};
 
 
 HBWDisplayLine::HBWDisplayLine(hbw_config_display_line* _config)
@@ -177,7 +178,7 @@ void HBWDisplayLine::set(HBWDevice* device, uint8_t length, uint8_t const * cons
       lastKeyNum = data[1];
   }
   
-  HBWDisplayDim::displayWakeUp = true;
+  HBWDimBacklight::displayWakeUp = true;
   
   if (length > 2 )  // need to set min. 3 characters (should be ok, to set "foo" or even a single variable, like "%1%")
   {
@@ -230,56 +231,56 @@ void HBWDisplayLine::set(HBWDevice* device, uint8_t length, uint8_t const * cons
   // 202 - auto_brightness off
   // 204 - auto_brightness on
   // 255 - toggle backligh
-void HBWDisplayDim::set(HBWDevice* device, uint8_t length, uint8_t const * const data)
-{
- #ifdef DEBUG_OUTPUT
- hbwdebug(F("setDim: "));
- #endif
-
-  if (length == 2)
-  {
-    if (lastKeyNum == data[1])  // ignore repeated key press
-      return;
-    else
-      lastKeyNum = data[1];
-  }
-
-  HBWDisplayDim::displayWakeUp = true;
-  
-  if (data[0] > 200)
-  {
-    switch (data[0]) {
-      case 202:  // auto_brightness off
-        config->auto_brightness = false;
-   #ifdef DEBUG_OUTPUT
-   hbwdebug(F("auto_brightness off"));
-   #endif
-        break;
-      case 204:  // auto_brightness on
-        config->auto_brightness = true;
-   #ifdef DEBUG_OUTPUT
-   hbwdebug(F("auto_brightness on"));
-   #endif
-        break;
-      case 255:   // toggle on/off, only when not in auto_brightness
-        if (config->auto_brightness == false)
-          currentValue = currentValue > 0 ? 0 : 200;
-   #ifdef DEBUG_OUTPUT
-   hbwdebug(F("toggle"));
-   #endif
-        break;
-    }
-  }
-  else {
-    currentValue = data[0];
-    config->auto_brightness = false;  // auto_brightness off when setting value manually
-  }
-  
-#ifdef DEBUG_OUTPUT
-hbwdebug(F(", currentValue: ")); hbwdebug(currentValue);
-hbwdebug(F("\n"));
-#endif 
-};
+//void HBWDisplayDim::set(HBWDevice* device, uint8_t length, uint8_t const * const data)
+//{
+// #ifdef DEBUG_OUTPUT
+// hbwdebug(F("setDim: "));
+// #endif
+//
+//  if (length == 2)
+//  {
+//    if (lastKeyNum == data[1])  // ignore repeated key press
+//      return;
+//    else
+//      lastKeyNum = data[1];
+//  }
+//
+//  HBWDisplayDim::displayWakeUp = true;
+//  
+//  if (data[0] > 200)
+//  {
+//    switch (data[0]) {
+//      case 202:  // auto_brightness off
+//        config->auto_brightness = false;
+//   #ifdef DEBUG_OUTPUT
+//   hbwdebug(F("auto_brightness off"));
+//   #endif
+//        break;
+//      case 204:  // auto_brightness on
+//        config->auto_brightness = true;
+//   #ifdef DEBUG_OUTPUT
+//   hbwdebug(F("auto_brightness on"));
+//   #endif
+//        break;
+//      case 255:   // toggle on/off, only when not in auto_brightness
+//        if (config->auto_brightness == false)
+//          currentValue = currentValue > 0 ? 0 : 200;
+//   #ifdef DEBUG_OUTPUT
+//   hbwdebug(F("toggle"));
+//   #endif
+//        break;
+//    }
+//  }
+//  else {
+//    currentValue = data[0];
+//    config->auto_brightness = false;  // auto_brightness off when setting value manually
+//  }
+//  
+//#ifdef DEBUG_OUTPUT
+//hbwdebug(F(", currentValue: ")); hbwdebug(currentValue);
+//hbwdebug(F("\n"));
+//#endif 
+//};
 
 
 /* set special input value for a channel, via peering event. */
@@ -287,17 +288,17 @@ void HBWDisplay::set(HBWDevice* device, uint8_t length, uint8_t const * const da
 {
   //displayWakeUp = true;
 // TODO: allow key peerig, to change entire screens?
-// or use peering of one key with all lines, to get alle changed?
+// or use peering of one key with all lines, to get all changed?
 };
 
 
 /* standard public function - returns length of data array. Data array contains current channel reading */
-uint8_t HBWDisplayDim::get(uint8_t* data)
-{
-  *data = currentValue;
-
-  return 1;
-};
+//uint8_t HBWDisplayDim::get(uint8_t* data)
+//{
+//  *data = currentValue;
+//
+//  return 1;
+//};
 
 
 /* standard public function - returns length of data array. Data array contains current channel reading */
@@ -553,52 +554,52 @@ void HBWDisplay::loop(HBWDevice* device, uint8_t channel)
 
 
 /* standard public function - called by device main loop for every channel in sequential order */
-void HBWDisplayDim::loop(HBWDevice* device, uint8_t channel)
-{
-  if (!initDone) {
-    if (config->startup)  currentValue = 200; // set startup value (200 = on, 0 = off)
-    initDone = true;
-    return;
-  }
-
-  uint32_t now = millis();
-
-  if (now - backlightLastUpdate > LCD_BACKLIGHT_UPDATE_INTERVAL || displayWakeUp)
-  {
-    backlightLastUpdate = now;
-    
-    if (displayWakeUp) {
-      displayWakeUp = false;
-      powerOnTime = now;   // reset timer for "auto_off"
-    }
-
-    if (config->auto_brightness && photoresistorPin != NOT_A_PIN)
-    {
-      int16_t brightnessDiff = (analogRead(photoresistorPin) >> 2) - brightness;
-      
-      // smooth ADC reading, but allow larger steps if backlight is bright, or the measured brightness changed a lot
-      if (brightnessDiff > 4) {
-        brightness += 1 + (currentValue/32) + (brightnessDiff/50);  // try using divider with power of 2, to use bitshift
-      }
-      else if (brightnessDiff < -4) {
-        brightness -= 1 + (currentValue/32) + ((brightnessDiff * -1)/50);
-      }
-      
-      currentValue = map(brightness, 0, 255, 0, 200);
-    }
-
-    // overwrite current value, to turn of the backlight, if auto-off applies
-    if (now - powerOnTime >= (uint32_t)config->auto_off *60000 && config->auto_off)
-      currentValue = 0;
-
-//hbwdebug(F("Dim LDR:")); hbwdebug(analogRead(photoresistorPin) >> 2);
-//hbwdebug(F(" brightness:")); hbwdebug(brightness);
-//hbwdebug(F(" currentValue:")); hbwdebug(currentValue);
-//hbwdebug(F("\n"));
-    
-    analogWrite(backlightPin, map(currentValue, 0, 200, 0, 255));
-  }
-};
+//void HBWDisplayDim::loop(HBWDevice* device, uint8_t channel)
+//{
+//  if (!initDone) {
+//    if (config->startup)  currentValue = 200; // set startup value (200 = on, 0 = off)
+//    initDone = true;
+//    return;
+//  }
+//
+//  uint32_t now = millis();
+//
+//  if (now - backlightLastUpdate > LCD_BACKLIGHT_UPDATE_INTERVAL || displayWakeUp)
+//  {
+//    backlightLastUpdate = now;
+//    
+//    if (displayWakeUp) {
+//      displayWakeUp = false;
+//      powerOnTime = now;   // reset timer for "auto_off"
+//    }
+//
+//    if (config->auto_brightness && photoresistorPin != NOT_A_PIN)
+//    {
+//      int16_t brightnessDiff = (analogRead(photoresistorPin) >> 2) - brightness;
+//      
+//      // smooth ADC reading, but allow larger steps if backlight is bright, or the measured brightness changed a lot
+//      if (brightnessDiff > 4) {
+//        brightness += 1 + (currentValue/32) + (brightnessDiff/50);  // try using divider with power of 2, to use bitshift
+//      }
+//      else if (brightnessDiff < -4) {
+//        brightness -= 1 + (currentValue/32) + ((brightnessDiff * -1)/50);
+//      }
+//      
+//      currentValue = map(brightness, 0, 255, 0, 200);
+//    }
+//
+//    // overwrite current value, to turn of the backlight, if auto-off applies
+//    if (now - powerOnTime >= (uint32_t)config->auto_off *60000 && config->auto_off)
+//      currentValue = 0;
+//
+////hbwdebug(F("Dim LDR:")); hbwdebug(analogRead(photoresistorPin) >> 2);
+////hbwdebug(F(" brightness:")); hbwdebug(brightness);
+////hbwdebug(F(" currentValue:")); hbwdebug(currentValue);
+////hbwdebug(F("\n"));
+//    
+//    analogWrite(backlightPin, map(currentValue, 0, 200, 0, 255));
+//  }
+//};
 
 
 // TODO: specialize template to keep separate .cpp and .h files.... if possible
