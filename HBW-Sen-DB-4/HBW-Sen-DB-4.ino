c2a45b2279e0b5057572acab911519975ec89368
loetmeister <loetmeister@habmalnefrage.de>
2024-12-20 10:45:40 +0100

    added peering for backlight channel

--- HBW-Sen-DB-4/HBW-Sen-DB-4.ino
+++ HBW-Sen-DB-4/HBW-Sen-DB-4.ino
@@ -16,77 +16,38 @@
 // - fix for repeatCounter reset handling
 // v0.3
 // - added buzzer for button feedback (uses tone() builtin function) - needs new XML due to additional config
+//   Set pin BUZZER NOT_A_PIN to disable it
+// v0.4
+// - added peering for backlight chan - needs new XML due to updated config!
 
 
 #define HARDWARE_VERSION 0x01
-#define FIRMWARE_VERSION 0x001E
+#define FIRMWARE_VERSION 0x0028
 #define HMW_DEVICETYPE 0x98 //device ID (make sure to import hbw-dis-key-4.xml into FHEM)
 
-// + 1 türsummer?
+//TODO? + 1 türsummer?
 #define NUMBER_OF_DIM_CHAN 1   // dimmer output (for backlight) - with auto_brightness
 #define NUMBER_OF_KEY_CHAN 4   // knobs at your door
 #define NUM_LINKS_KEY 20
-#define LINKADDRESSSTART_V_CHAN 0x30  // any actor peering!
+#define NUM_LINKS_DIM 20
+#define LINKADDRESSSTART_DIM 0x40  // any actor peering!
 #define LINKADDRESSSTART_KEY 0x13C  // any sensor peering!
-
-//#define USE_HARDWARE_SERIAL   // use hardware serial (USART) for final device - this disables debug output
-/* Undefine "HBW_DEBUG" in 'HBWired.h' to remove code not needed. "HBW_DEBUG" also works as master switch,
- * as hbwdebug() or hbwdebughex() used in channels will point to empty functions. */
 
 
 // HB Wired protocol and module
 #include <HBWired.h>
+#include <HBW_eeprom.h>
 #include <HBWLinkKey.h>
 #include "HBWKeyDoorbell.h"
 #include <HBWDimBacklight.h>
+#include <HBWLinkSwitchSimple.h>
 
 
-// Pins
-#ifdef USE_HARDWARE_SERIAL
-  #define RS485_TXEN 2  // Transmit-Enable
-  #define BUTTON A6  // Button fuer Factory-Reset etc.
-  
-  #define BUTTON_1 8
-  #define BUTTON_2 7
-  #define BUTTON_3 4
-  #define BUTTON_4 A1
-
-  #define BUZZER 11 // buzzer for key press feedback
-  #define BACKLIGHT_PWM 9  // timer1 (pin 9 & 10)
-  #define LDR_PIN A0
-  
-//  #define BLOCKED_TWI_SDA A4  // used by I²C - SDA
-//  #define BLOCKED_TWI_SCL A5  // used by I²C - SCL
-  
-#else
-  #define RS485_RXD 4
-  #define RS485_TXD 2
-  #define RS485_TXEN 3  // Transmit-Enable
-  #define BUTTON 8  // Button fuer Factory-Reset etc.
-
-  #define BUTTON_1 A0
-  #define BUTTON_2 A1
-  #define BUTTON_3 A2
-  #define BUTTON_4 A3
-  
-  #define BUZZER 11 // buzzer for key press feedback  (tone() timer prio: timer2, 1, 0)
-  // 11, 3 (controlled by timer2), timer0 (pin 5 & 6), timer1 (pin 9 & 10)
-  #define BACKLIGHT_PWM 9
-  #define LDR_PIN A7
-  
-//  #define BLOCKED_TWI_SDA A4  // used by I²C - SDA
-//  #define BLOCKED_TWI_SCL A5  // used by I²C - SCL
-
-  #include "FreeRam.h"
-  #include "HBWSoftwareSerial.h"
-  HBWSoftwareSerial rs485(RS485_RXD, RS485_TXD); // RX, TX
-#endif  //USE_HARDWARE_SERIAL
-
-#define LED LED_BUILTIN        // Signal-LED
+// Pins and hardware config
+#include "HBW-Sen-DB-4_config_example.h"  // When using custom device pinout or controller, copy this file and include it instead
 
 
 #define NUMBER_OF_CHAN NUMBER_OF_DIM_CHAN + NUMBER_OF_KEY_CHAN
-
 
 struct hbw_config {
   uint8_t logging_time;     // 0x01
@@ -106,7 +67,6 @@ HBWDevice* device = NULL;
 void setup()
 {
   channels[0] = new HBWDimBacklight(&(hbwconfig.BlDimCfg[0]), BACKLIGHT_PWM, LDR_PIN);
-
   
   static const byte BUTTON_PIN[] = {BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4};
   
@@ -127,7 +87,7 @@ void setup()
                              NUMBER_OF_CHAN, (HBWChannel**)channels,
                              NULL,
                              new HBWLinkKey<NUM_LINKS_KEY, LINKADDRESSSTART_KEY>(),
-                             NULL
+                             new HBWLinkSwitchSimple<NUM_LINKS_DIM, LINKADDRESSSTART_DIM>()
                              );
   
   device->setConfigPins(BUTTON, LED);  // use analog input for 'BUTTON'
@@ -141,7 +101,7 @@ void setup()
                              NUMBER_OF_CHAN, (HBWChannel**)channels,
                              &Serial,
                              new HBWLinkKey<NUM_LINKS_KEY, LINKADDRESSSTART_KEY>(),
-                             NULL
+                             new HBWLinkSwitchSimple<NUM_LINKS_DIM, LINKADDRESSSTART_DIM>()
                              );
   
   device->setConfigPins(BUTTON, LED);  // 8 (button) and 13 (led) is the default
